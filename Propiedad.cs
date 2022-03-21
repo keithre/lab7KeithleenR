eb101bb424ecda4dcdf715fba8dc288bcc672129
keithre <ivana2003@mesoamericana.edu.gt>
2022-03-20 23:19:29 -0600

    cambiosAgregar archivos de proyecto.

---
+++ Propiedad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7KeithleenR
+{
+    class Propiedad
+    {
+        public int NoCasa { get; set; }
+        public string DPI { get; set; }
+        public decimal CuotaMantenimiento { get; set; }
+    }
+}
---
+++ Resumen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7KeithleenR
+{
+    class Resumen
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NoCasa { get; set; }
+        public decimal CuotaMantenimiento { get; set; }
+    }
+}
